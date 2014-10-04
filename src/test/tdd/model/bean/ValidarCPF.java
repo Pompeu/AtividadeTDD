53d2af9df0fd399f11788a1e91f348bf6c9abd4f
Pompeu <pompeu@pompeu-System-Product-Name.(none)>
2014-10-04 03:10:05 -0300

    classe validar cpf on

--- src/test/tdd/model/bean/ValidarCPF.java
+++ src/test/tdd/model/bean/ValidarCPF.java
@@ -3,11 +3,11 @@ package test.tdd.model.bean;
 public class ValidarCPF {
 	public static boolean validarCPF(String cpf) {
 		if (!cpf.matches("\\d{11}") || cpf.length() != 11
-				|| cpf == "00000000000" || cpf == "11111111111"
-				|| cpf == "22222222222" || cpf == "33333333333"
-				|| cpf == "44444444444" || cpf == "55555555555"
-				|| cpf == "66666666666" || cpf == "77777777777"
-				|| cpf == "88888888888" || cpf == "99999999999") {
+				|| cpf.equals("00000000000") || cpf.equals("11111111111")
+				|| cpf.equals("22222222222") || cpf.equals("33333333333")
+				|| cpf.equals("44444444444") || cpf.equals("55555555555")
+				|| cpf.equals("66666666666") || cpf.equals("77777777777")
+				|| cpf.equals("88888888888") || cpf.equals("99999999999")) {
 			return false;
 
 		}
